537e6f082c7914227d620edec703f985b8867177
Grant Timmerman <timmerman+devrel@google.com>
2019-11-07 12:13:33 -0500

    fix: simplify Run deployment and add developer TODO for GCF

--- scripts.sh
+++ scripts.sh
@@ -16,10 +16,10 @@ deploy_to_run() {
   gcloud components install beta
   GCP_PROJECT=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
   gcloud config set run/region us-central1
+  gcloud config set run/platform managed
   gcloud builds submit --tag gcr.io/$GCP_PROJECT/tasks-pizza
   gcloud beta run deploy tasks-pizza \
     --image gcr.io/$GCP_PROJECT/tasks-pizza \
-    --platform managed \
     --allow-unauthenticated
   echo "Deployed $(gcloud beta run routes describe tasks-pizza --platform managed --format 'value(status.address.url)')"
 }
